22020aef0bc924c6845931345b699207079eaa77
radhakrishna-programmer <81542600+radhakrishna-programmer@users.noreply.github.com>
2021-11-29 10:31:05 +0530

    Update insertion.java

--- sorting files/insertion.java
+++ sorting files/insertion.java
@@ -9,15 +9,14 @@ public class insertion {
     for( i=0;i<n;i++){
         arr[i]=input.nextInt();
     }
-    for(i=1;i<=n-1;i++){
-        k=arr[i];
-        j=i;
-        while(arr[j-1]>k &&j>=1){
-            arr[j]=arr[j-1];
-            j--;
-
+    for(i=0;i<n-1;i++){
+        for(j=i+1;j>0;j--){
+            if(arr[j]<arr[j-1]){
+                k=arr[j];
+                arr[j]=ar[j-1];
+                arr[j-1]=k;
+            }
         }
-        arr[j]=k;
     }
     for( i=0;i<n;i++){
         System.out.println(arr[i]);
